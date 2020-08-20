42b2373943c9db186b6d191b637552e00b1bbe39
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-08-20 15:48:52 -0400

    Set up font awesome and additional styles.

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,9 @@
 					<nav>
 						<ul>
 							<li><a href="#intro">Welcome</a></li>
-							<li><a href="#one">Projects</a></li>
-							<li><a href="#two">Skills</a></li>
-							<li><a href="#three">Get in touch</a></li>
+							<li><a href="#projects">Projects</a></li>
+							<li><a href="#skills">Skills</a></li>
+							<li><a href="#contact">Get in touch</a></li>
 						</ul>
 					</nav>
 				</div>
@@ -20,79 +20,93 @@
 					<section id="intro" class="wrapper style1 fullscreen fade-up">
 						<div class="inner">
 							<h1>Nicholas Buzzy, J.D.</h1>
-							<p>Software developer and lawyer, but I am always an entrepreneur at heart.</p>
+							<p>Experienced senior software developer.  Former litigator.  Always an entrepreneur.</p>
 							<ul class="actions">
-								<li><a href="#one" class="button scrolly">Learn more</a></li>
+								<li><a href="#projects" class="button scrolly">Learn more</a></li>
 							</ul>
 						</div>
 					</section>
 
 				<!-- Projects -->
-					<section id="one" class="wrapper style2 spotlights">
+					<section id="projects" class="wrapper style2 spotlights">
 						<section id="project-one">
+							<a id="test" href="#" class="image"><img src="../public/assets/images/buzzycite.jpg" alt="" data-position="center center" /></a>
 							<div class="content">
 								<div class="inner">
 									<h2>BuzzyCite</h2>
-									<p>Founder and CEO.  Created the largest free search engine for court cases.  Developed algorithm that correctly cites court cases.  Built with MEAN stack.  </p>
+									<p>Founder and CEO.  Created the largest free search engine for court cases.  Developed algorithm that correctly cites court cases.  
+										Featured by 
+										<a	class="buzzycite-media-link"
+											target="_blank"
+											href="https://case.law/gallery/applications-third-party"
+										>
+											Harvard Law School</a> and 
+										<a	class="buzzycite-media-link" 
+											target="_blank"
+											href="https://www.lawsitesblog.com/2020/06/new-legal-research-tool-taps-into-caselaw-access-projects-6-7m-cases.html"
+										>
+											LawSites</a>.
+									</p>
 									<ul class="actions">
-										<li><a href="https://buzzycite.com" target="_blank" class="button">Learn more</a></li>
+										<li><a href="https://buzzycite.com" target="_blank" class="button">See BuzzyCite</a></li>
 									</ul>
 								</div>
 							</div>
 						</section>
 						<section id="project-two">
+							<a href="#" class="image"><img src="../public/assets/images/buzzyprice.jpg" alt="" data-position="top center" /></a>
 							<div class="content">
 								<div class="inner">
 									<h2>Buzzy Price</h2>
 									<p>Founder and CEO.  Built the ultimate eBay price tracker where we allow users to set custom alerts for price updates.</p>
 									<ul class="actions">
-										<li><a href="https://buzzyprice.com" class="button">Learn more</a></li>
+										<li><a href="https://buzzyprice.com" target="_blank" class="button">See Buzzy Price</a></li>
 									</ul>
 								</div>
 							</div>
 						</section>
 					</section>
 				<!-- Skills -->
-					<section id="two" class="wrapper style3 fade-up">
+					<section id="skills" class="wrapper style3 fade-up">
 						<div class="inner">
 							<h2>Skills</h2>
-							<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet imperdiet est velit quis lorem.</p>
+							<p>Six years litigation experience including thirteen jury trials.  Founder and CEO of two startup businesses.  Below are technical skills used to develop enterprise applications.</p>
 							<div class="features">
 								<section>
-									<span class="icon solid major fa-code"></span>
-									<h3>Lorem ipsum amet</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+									<span class="icon solid major"><i class="fab fa-angular"></i></span>
+									<h3>Angular</h3>
+									<p>Use for enterprise applications such as BuzzyCite and implement its observables, dependency injection, and TypeScript models.</p>
 								</section>
 								<section>
-									<span class="icon solid major fa-lock"></span>
-									<h3>Aliquam sed nullam</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+									<span class="icon solid major"><i class="fab fa-vuejs"></i></span>
+									<h3>Vue.js</h3>
+									<p>Lightweight front end framework used with Buzzy Price and this portfolio page.  State management with Vuex.</p>
 								</section>
 								<section>
-									<span class="icon solid major fa-cog"></span>
-									<h3>Sed erat ullam corper</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+									<span class="icon solid major"><i class="fab fa-node"></i></span>
+									<h3>Node.js</h3>
+									<p>Implement this server technoology with BuzzyCite and Buzzy Price.  Express framework for API route management and database connections.</p>
 								</section>
 								<section>
-									<span class="icon solid major fa-desktop"></span>
-									<h3>Veroeros quis lorem</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+									<span class="icon solid major"><i class="fab fa-python"></i></span>
+									<h3>Python</h3>
+									<p>Automation, data science, and web scraping for various applications.  Flask framework for building APIs.</p>
 								</section>
 								<section>
-									<span class="icon solid major fa-link"></span>
-									<h3>Urna quis bibendum</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+									<span class="icon solid major fa-database"></span>
+									<h3>MongoDB</h3>
+									<p>Database for BuzzyCite.  Manage users and citation data.  Enforce authentication and usage limits.</p>
 								</section>
 								<section>
-									<span class="icon major fa-gem"></span>
-									<h3>Aliquam urna dapibus</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+									<span class="icon solid major"><i class="fab fa-css3"></i></span>
+									<h3>HTML5/CSS3</h3>
+									<p>Extensive use with every project.  Additionally, utilize SCSS for BuzzyCite and SASS with Buzzy Price.</p>
 								</section>
 							</div>
 						</div>
 					</section>
 				<!-- Get in Touch -->
-					<section id="three" class="wrapper style1 fade-up">
+					<section id="contact" class="wrapper style1 fade-up">
 						<div class="inner">
 							<h2>Get in touch</h2>
 							<!-- <p>.</p> -->
@@ -121,11 +135,11 @@
 								<section>
 									<ul class="contact">
 										<li>
-											<h3>Social</h3>
+											<h3 class="social">Social</h3>
 											<ul class="icons">
-												<li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
-												<li><a href="#" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
-												<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+												<li><a href="https://www.linkedin.com/in/nicholas-buzzy/" target="_blank" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+												<li><a href="https://github.com/nbbuzzy1" target="_blank" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+												<li><a href="https://www.facebook.com/nbbuzzy23" target="_blank" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
 											</ul>
 										</li>
 									</ul>
@@ -144,6 +158,15 @@ export default {
 </script>
 
 <style>
+
+html {
+	scroll-behavior: smooth
+}
+
+body {
+	background: #D1E8E2 !important;
+}
+
 .wrapper.style1 {
     background-color: #116466;
 }
@@ -157,15 +180,13 @@ export default {
 	/* border-bottom: 1px solid #D1E8E2; */
 }
 
-@media(min-width: 1280px) {
-	#sidebar nav a {
-		border-bottom: 1px solid #D1E8E2;
-	}
-}
-
 #project-one {
 	background: #ffcb9a;
 	color: #2C3531;
+}
+
+.spotlights > #project-one > .image:before {
+		background: #ffcb9a !important;
 }
 
 #project-one h2, #project-two h2, #project-one a.button, #project-two a.button {
@@ -182,6 +203,23 @@ export default {
 	color: #2C3531;
 }
 
+.spotlights > #project-two > .image:before {
+	background: #d9b08c !important;
+}
+
+#project-one img, #project-two img {
+	display: block;
+    max-width: 100%;
+    max-height: 450px;
+    width: auto;
+    height: auto;
+}
+
+#project-one .image, #project-two .image {
+	display: flex;
+    align-items: center;
+}
+
 .wrapper.style3 {
 	background: #d1e8e2;
 	color: #2c3531;
@@ -189,5 +227,28 @@ export default {
 
 .wrapper.style3 h2, .wrapper.style3 h3, .wrapper.style3 .icon, .wrapper.style3 .icon.major:before {
 	color: #2c3531 !important;
+}
+
+.contact .icons .icon {
+	font-size: 30px;
+	margin-right: 10px;
+}
+
+@media(min-width: 1280px) {
+	#sidebar nav a {
+		border-bottom: 1px solid #D1E8E2 !important;
+	}
+}
+
+@media(max-width: 980px) {
+	#project-one img, #project-two img {
+		max-width: 100%;
+		max-height: 100%;
+		margin-top: 50px;
+	}
+
+	#project-one .image, #project-two .image {
+		justify-content: center;
+	}
 }
 </style>
